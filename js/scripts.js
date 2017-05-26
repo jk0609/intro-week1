f3457e48f21012648f1319991e0ae41fa7f9498a
JonathonKang <jonkang@hotmail.com>
2017-05-26 15:26:37 -0700

    Update the About Me section with finished content, improve layout of portfolio section and update 3/4 sections with existing projects.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,36 @@
 $(document).ready(function(){
+
+//Fix navbar
+//finalize about me content
+//github link in div
+//check if its good on mobile
+
   $('.arrows').click(function(){
     var whichPort = this['alt'];
     $('.arrows[alt='+whichPort+']').toggle();
     var thisPort = $('.portSection[value='+whichPort+']')
     if(thisPort.is(':visible')){
       thisPort.children().toggle();
+      thisPort.next().fadeToggle();
     }
     else{
       thisPort.children().delay(500).fadeToggle();
+      thisPort.next().delay(500).fadeToggle();
     }
     thisPort.slideToggle();
-
   });
+
+  $('a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+
+    var target = this.hash,
+        $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top - 50
+    }, 500, 'swing', function () {
+        window.location.hash = target;
+    });
+  });
+
 });
